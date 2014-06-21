d3c95ed9f99f9858a0b2e8ad65d8388bae0a6068
Luka <luka.toni93@gmail.com>
2014-06-22 00:00:32 +0200

    Create Factorial.c

---
+++ Factorial.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j, fact;
+    for(i = 1; i <= 7; i++)
+    {
+        fact = 1;
+        for(j = 1; j <= i; j++)
+        {
+            fact = fact *j;
+            if(j != i)
+                printf("%d * ", j);
+            else
+                printf("%d = ", j);
+        }
+        printf("%d\n",fact);
+    }
+    return 0;
+}
